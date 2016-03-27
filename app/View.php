e328a46f92f72e5e5b2c1aceac02db2cffcdb843
Maxim Vlasov <sergeich06@gmail.com>
2016-03-27 10:47:14 +0300

    Added templates

--- app/View.php
+++ app/View.php
@@ -3,21 +3,54 @@ namespace App\View;
 
 class View{
 
-    public $data = '1231231';
-
-    public function render($templates, $data = null)
-    {
-        if(is_array($data)) {
-
-            extract($data);
-        }
-        foreach($templates as $template){
-            include_once 'views/'.$template.'.html.php';
+    private $vars = array();
+    private $tpldir = 'views/';
+    private $content_type = '';
+    public function __get( $name ){
+        if( isset($this->vars[$name]) )
+            return $this->vars[$name];
+    }
+    public function __set( $name,$value ){
+        $this->vars[$name] = $value;
+    }
+    private function safe( $file ){
+        if( file_exists($file) ){
+            return file_get_contents($file);
+        }else{
+            console( 'Файл '.$file.' не найден ',__FILE__,__CLASS__,__LINE__ );
+            return '';
         }
     }
+    private function exec($file) {
+        eval('?>'.$this->safe( ROOT.$this->tpldir.$file.'.tpl') );
+    }
 
-    public function layer($name){
-        include_once 'views/'.$name;
+    public function assign( $var, $val='' ) {
+        if( is_scalar($var) )
+            $this->vars[$var] = $val;
+        else
+            $this->vars = array_merge($this->vars,$var);
+    }
+    public function parse( $file,$vars = array() ) {
+        ob_start();
+        $this->show( $file,$vars );
+        return ob_get_clean();
+    }
+    public function read( $file ){
+        return $this->safe( ROOT.$this->tpldir.$file.'.tpl' );
+    }
+
+    public function show( $file,$vars = array() ) {
+        $this->assign($vars);
+        extract($this->vars);
+        $this->exec( $file );
+    }
+    function setContentType( $type ){
+        if( in_array(strtolower($type),array('page','json','html')) )
+            $this->content_type = strtolower($type);
+    }
+    function getContentType( ){
+        return $this->content_type;
     }
 
 }
